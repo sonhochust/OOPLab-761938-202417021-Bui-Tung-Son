b6ec0ffce90207147ff194250b133ccca4d6563a
sonhochust <toitramzn123@example.com>
2026-03-28 20:00:27 +0700

    bai 6.6

---
+++ src/week1/bai66/src/bai66/bai66.java
@@ -0,0 +1,41 @@
+package bai66;
+import java.util.Scanner;
+
+public class bai66
+ {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so dong: ");
+        int r = sc.nextInt();
+        System.out.print("Nhap so cot: ");
+        int c = sc.nextInt();
+        int[][] matrix1 = new int[r][c];
+        int[][] matrix2 = new int[r][c];
+        int[][] sumMatrix = new int[r][c];
+        System.out.println("Ma tran 1:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Ma tran 2:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("Ma tran ket qua:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(sumMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
