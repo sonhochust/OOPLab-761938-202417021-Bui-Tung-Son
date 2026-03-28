7d0f7b83aa8dbc9174b21868c80ca7212bed86db
sonhochust <toitramzn123@example.com>
2026-03-28 20:37:52 +0700

    update bai 6.5

--- src/week1/bai65/src/bai65/bai65.java
+++ src/week1/bai65/src/bai65/bai65.java
@@ -2,31 +2,32 @@ package bai65;
 import java.util.Scanner;
 
 public class bai65 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Moi nhap so so hang: ");
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		long sum=0;
-		for(int i=0;i<n;i++) {
-			a[i]=sc.nextInt();
-			sum+=a[i];
-		}
-		for(int i=0;i<n;i++) {
-			for(int j=i+1;j<n;j++) {
-				if(a[i]>a[j]) {
-					int t=a[i];
-					a[i]=a[j];
-					a[j]=t;
-				}
-			}
-		}
-		System.out.print("Day so sau sap xep: ");
-		for(int i=0;i<n;i++) {
-			System.out.print(a[i]+" ");
-		}
-		System.out.print("\n");
-		System.out.println("Tong = " + sum);
-		System.out.println("Trung binh = " + (sum/n));
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Moi nhap so so hang: ");
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    long sum = 0;
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+      sum += a[i];
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (a[i] > a[j]) {
+          int t = a[i];
+          a[i] = a[j];
+          a[j] = t;
+        }
+      }
+    }
+    System.out.print("Day so sau sap xep: ");
+    for (int i = 0; i < n; i++) {
+      System.out.print(a[i] + " ");
+    }
+    System.out.print("\n");
+    System.out.println("Tong = " + sum);
+    System.out.println("Trung binh = " + (sum / n));
+    sc.close();
+  }
 }
